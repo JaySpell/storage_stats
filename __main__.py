3f372e523fdb717a0529f046388fece3b5e67c29
Krusty <jspell@gmail.com>
2016-04-25 13:57:52 -0500

    chart working

--- __main__.py
+++ __main__.py
@@ -1,7 +1,36 @@
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import *
+from numpy import pi
+import parse_utils
 
 def graph_it():
     output_file("test.html")
     p = figure(plot_width=400, plot_height=400)
     p.line([1, 2, 3, 4, 5], [6, 7, 8, 9, 10], line_width=2)
+    show(p)
+
+def pie_it(percents, starts, ends):
+    # define starts/ends for wedges from percentages of a circle
+
+    #percents = [0, 0.3, 0.4, 0.6, 0.9, 1]
+
+    #Get list of storage stats
+    storage_stats = paruse_utils.group_totals(['tmcv7000-4', 'tmcv7000-3'])
+
+    #Get totals and convert to %
+    percents = parse_utils.space_percent(storage_stats, sel_out="list")
+    percents.append(1)
+
+    #Get list instead of dict
+    starts = [p*2*pi for p in percents[:-1]]
+    ends = [p*2*pi for p in percents[1:]]
+
+    # a color for each pie piece
+    colors = ["red", "green", "blue", "orange", "yellow"]
+
+    p = figure(x_range=(-1,1), y_range=(-1,1))
+
+    p.wedge(x=0, y=0, radius=1, start_angle=starts, end_angle=ends, color=colors)
+
+    # display/save everythin
+    output_file("pie.html")
     show(p)
--- parse_utils.py
+++ parse_utils.py
@@ -1,6 +1,6 @@
 CURRENT_FILE = "/home/kcup/python/graph/output"
 
-def group_totals(strg_list):
+def group_totals(storage):
     free_total = int(0)
     used_total = int(0)
     space_total = int(0)
@@ -8,11 +8,11 @@ def group_totals(strg_list):
     u_total = []
     s_total = []
 
-    for str_sys in strg_list:
+    for storage_system in storage:
         a_file = str_file_open()
         for line in a_file:
             lsplit = line.split(',')
-            if lsplit[1] in str_sys:
+            if lsplit[1] in storage_system:
                 free_total = int(free_total) + int(lsplit[7])
                 used_total = int(used_total) + int(lsplit[9])
                 space_total = free_total + used_total
@@ -20,22 +20,37 @@ def group_totals(strg_list):
         u_total.append(used_total)
         s_total.insert(0, space_total)
         a_file.close()
-    r_totals = {'free': [sum(f_total)],
+    all_totals = {'free': [sum(f_total)],
             'used': [sum(u_total)],
             'space': [sum(s_total)]}
-    return r_totals
+    return all_totals
 
-def space_percent(r_totals):
-    s_totals = r_totals['space'][0]
+def space_percent(all_totals, sel_out="dict"):
+    '''
+    Takes the list of values and returns the % value.
+    Input should be a dict with the following
+    {
+        'used': [used_float],
+        'free': [free_float],
+        'space': [total of the two]
+    }
+    '''
+    s_totals = all_totals['space'][0]
+
     r_percent = lambda x: float(x)/float(s_totals)
+    n_list = list(map(r_percent, all_totals['free']))
+    all_totals['free'] = n_list.pop(0)
+    n_list = list(map(r_percent, all_totals['used']))
+    all_totals['used'] = n_list.pop(0)
 
-    n_list = map(r_percent, r_totals['free'])
-    r_totals['free'] = n_list.pop(0)
-
-    n_list = map(r_percent, r_totals['used'])
-    r_totals['used'] = n_list.pop(0)
-
-    return r_totals
+    if sel_out == "list":
+        rn_list = [
+        all_totals['free'],
+        all_totals['used'],
+        ]
+        all_totals = rn_list
+        
+    return all_totals
 
 def str_file_open(current_file=CURRENT_FILE):
     a_file = open(current_file, 'r')
